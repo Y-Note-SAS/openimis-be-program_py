5f675d2e13ffc297d1eb0749e5e15b26c92ecce6
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-02-17 14:01:23 +0100

    retrait du double filtre sur les polices expirées

--- program/schema.py
+++ program/schema.py
@@ -51,7 +51,7 @@ class Query(graphene.ObjectType):
             insuree = insuree_models.Insuree.objects.get(id=insuree_id)
             family_id = insuree.family_id
             policies = policy_models.Policy.objects.filter(
-                family_id=family_id).filter(expiry_date__gte=visit_date_from).filter(
+                family_id=family_id).filter(
                     start_date__lte=visit_date_from).filter(status__in=([2,8])) 
             program_products_ids = []
             products_programs = []
